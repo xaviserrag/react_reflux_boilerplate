7407b51ece958e070ba6a4963cecab6152d6c4f1
Carles Núñez <carles.nunez@zitrointeractive.com>
2015-07-23 15:03:21 +0200

    (FEAT) Changed some discordant names to be more code clearly.

--- src/components/ClickableArea.js
+++ src/components/ClickableArea.js
@@ -1,27 +1,27 @@
 'use strict';
 
 import React from 'react';
-import PlayerEntity from '../components/PlayerEntity.js';
+import PlayerItem from '../components/PlayerItem.js';
 
 export default React.createClass({
     //We define the propTypes for improve reusability
     propTypes: {
         players: React.PropTypes.array
     },
-    getEntities: function () {
-        let entities = [],
+    getItems: function () {
+        let items = [],
             i, len = this.props.players.length;
 
         for (i = 0; i < len; i++) {
-            entities.push(<PlayerEntity key={this.props.players[i].name} player={this.props.players[i]}/>);
+            items.push(<PlayerItem key={this.props.players[i].name} player={this.props.players[i]}/>);
         }
-        return entities;
+        return items;
     },
 
     render() {
         return (
             <ul className="clickableArea">
-                {this.getEntities()}
+                {this.getItems()}
             </ul>
         );
     }
